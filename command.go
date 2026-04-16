1968ad476e0b1db19d0f184c3b2a3543adb51619
weilok <weilok.chia@redtone.com>
2026-04-16 11:19:14 +0800

    refactor: introduce commandHandler type and newCommands constructor for cleaner CLI wiring

--- command.go
+++ command.go
@@ -15,17 +15,38 @@ type command struct {
 	args []string
 }
 
-type commands struct {
-	commandList map[string]func(*state, command) error
+type commandHandler func(*state, command) error                                                                   
+   
+type commands struct {                                                                                            
+	handlers map[string]commandHandler
+}                                                                                                                 
+
+func newCommands() *commands {                                                                                    
+	return &commands{handlers: make(map[string]commandHandler)}
 }
 
 func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) == 0 {
+	if len(cmd.args) < 1 {
 		return errors.New("The login handler expects a single argument, the username.")
 	}
 	if err := s.config.SetUser(cmd.args[0]); err != nil {
 		return err
 	}
 	fmt.Println("Username has been set!")
+	return nil
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	// This method runs a given command with the provided state if it exists.
+	funcHandler, ok := c.handlers[cmd.name]
+	if !ok {
+		return errors.New("Command not exist in this app!")
+	}
+	return funcHandler(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) error {
+	// This method registers a new handler function for a command name.
+	c.handlers[name] = f
 	return nil
 }
--- main.go
+++ main.go
@@ -3,13 +3,32 @@ package main
 import (
 	"fmt"
 	"github.com/weilok2021/blog-aggregator/internal/config"
+	"os"
 )
 
 func main() {
-	cfg, _ := config.Read() 
-	s := state{config: &cfg}
-	c := command{name: "login", args: []string{"Wei Lok"}}
+	if len(os.Args) < 2 {
+		fmt.Println("At least 2 CLI arguments are needed!")
+		os.Exit(1)
+	}
 
-	handlerLogin(&s, c)
+	cfg, _ := config.Read() 
+	appState := &state{config: &cfg}
+	cmds := newCommands()                                                                                             
+	cmds.register("login", handlerLogin)             
+
+	cmd := command{name: os.Args[1], args: os.Args[2:]}
+	err := cmds.run(appState, cmd)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	fmt.Println(cfg)
+	// start CLI application
+	// for {
+	// 	if len(os.Args) < 2 {
+	// 		fmt.Errorf("At least 2 CLI arguments are needed!")
+	// 		os.Exit(1)
+	// 	}
+	// }
 }
