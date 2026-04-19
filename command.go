4eb6c5f36c2bcdad443dbf92328ec03567e9703c
weilok2021 <weilok629@gmail.com>
2026-04-19 13:48:25 +0800

    feat: integrate postgres with sqlc and add user management commands
    - Add sqlc config and generated database package (users table)
    - Add SQL schema/queries for create, get, list, and reset users
    - Wire up database connection in main with lib/pq driver
    - Add register, reset, and users command handlers
    - Update login handler to verify user exists in database

--- command.go
+++ command.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"github.com/weilok2021/blog-aggregator/internal/config"
+	"github.com/weilok2021/blog-aggregator/internal/database"
 	"errors"
 	"fmt"
+	"time"
+	"context"
+	"os"
+	"github.com/google/uuid"
+
 )
 
 type state struct {
+	db  *database.Queries
 	config *config.Config
 }
 
@@ -25,14 +32,86 @@ func newCommands() *commands {
 	return &commands{handlers: make(map[string]commandHandler)}
 }
 
+// To handle user login, if user already existed in db, exit with status 1
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) < 1 {
 		return errors.New("The login handler expects a single argument, the username.")
 	}
+	
+	ctx := context.Background()
+	// Exit with code 1 if a user not exist in the database.
+	if _, err := s.db.GetUser(ctx, cmd.args[0]); err != nil {
+		fmt.Println("Couldn't login, no user record in database!")
+		os.Exit(1)
+	}
+
 	if err := s.config.SetUser(cmd.args[0]); err != nil {
 		return err
 	}
 	fmt.Println("Username has been set!")
+	return nil
+}
+
+// To register a new user in users table
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return errors.New("The register handler expects a single argument, the username.")
+	}
+
+	ctx := context.Background()
+	// Exit with code 1 if a user with that name already exists.
+	if _, err := s.db.GetUser(ctx, cmd.args[0]); err == nil {
+		fmt.Println("User with this name already existed in database!")
+		os.Exit(1)
+	}
+
+	createdUser, err := s.db.CreateUser(ctx, database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name: cmd.args[0],
+	})
+	if err != nil {
+		return err
+	}
+
+	s.config.SetUser(cmd.args[0])
+	fmt.Println("User has created! This is your information.")
+	fmt.Println(createdUser)
+	return nil
+}
+
+// To delete all users from users table
+func handlerReset(s *state, cmd command) error {
+	if len(cmd.args) != 0 {
+		return errors.New("The resit command expects no argument.")
+	}
+	ctx := context.Background()
+	if err := s.db.Reset(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func handlerListUsers(s *state, cmd command) error {
+	if len(cmd.args) != 0 {
+		return errors.New("The users command expects no argument.")
+	}
+	
+	ctx := context.Background()
+	users, err := s.db.GetUsers(ctx)
+	if err != nil {
+		fmt.Println("Users table not existed in db!")
+		return err
+	}
+
+	for _, user := range users {
+		fmt.Printf("* %s ", user.Name)
+		if user.Name == s.config.CurrentUserName {
+			fmt.Printf("(current)")
+		}
+		fmt.Printf("\n")
+	} 
 	return nil
 }
 
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
+import _ "github.com/lib/pq"
+
 import (
 	"fmt"
 	"github.com/weilok2021/blog-aggregator/internal/config"
 	"os"
+	"database/sql"
+	"github.com/weilok2021/blog-aggregator/internal/database"
 )
 
 func main() {
@@ -14,12 +18,24 @@ func main() {
 
 	cfg, _ := config.Read() 
 	appState := &state{config: &cfg}
+
+	// Create database connection
+	db, err := sql.Open("postgres", cfg.DbURL)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	appState.db = database.New(db) // we use queries through state.db
+
 	cmds := newCommands()                                                                                             
 	cmds.register("login", handlerLogin)             
+	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerListUsers)
+
 
 	cmd := command{name: os.Args[1], args: os.Args[2:]}
-	err := cmds.run(appState, cmd)
-	if err != nil {
+	if err := cmds.run(appState, cmd); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
